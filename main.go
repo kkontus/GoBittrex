3681c2e9d3592c889444af31d31a8a4ba11e2577
Kristijan Kontus <kontus@Kristijans-MacBook-Pro.local>
2017-10-21 16:51:23 +0100

    code and project structure reorganization

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    btrexhttp "GoBittrex/http"
+)
+
+func main() {
+    fmt.Printf("Hello Bittrex\n")
+
+    // btrexhttp.ExportedMethod();
+    // btrexhttp.GetGolangData();
+    btrexhttp.GetGolangDataJson();
+}
